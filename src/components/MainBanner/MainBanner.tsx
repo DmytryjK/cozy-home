11ac3aad7615c880ad030c692f3c10566367f0e9
Andrii Hlukhov <110923640+andriihlukhov@users.noreply.github.com>
2023-06-23 18:08:31 +0300

    FIX(home-page): UI fixes for MainBanner (#18)
    Jira: [[FE] UI fixes for MainBanner](https://cozyhometeam.atlassian.net/browse/CH-74)

--- src/components/MainBanner/MainBanner.tsx
+++ src/components/MainBanner/MainBanner.tsx
@@ -3,23 +3,28 @@ import './MainBanner.scss';
 const MainBanner = () => {
     return (
         <div className="mainBanner">
-            <div className="mainBanner__content">
-                <h2 className="mainBanner__content_subtitle">NEW ARRIVALS</h2>
-                <h1 className="mainBanner__content_title">
-                    TRENDY LOFT FURNITURE
-                </h1>
-                <p className="mainBanner__content_text">
-                    Відчуйте привабливість нашої колекції Trendy Loft Furniture,
-                    яка ідеально поєднує в собі елегантність і сучасність.{' '}
-                </p>
-                <a href="/">
-                    <button
-                        className="mainBanner__content_button"
-                        type="button"
-                    >
-                        Детальніше
-                    </button>
-                </a>
+            <div className="container">
+                <div className="mainBanner__content">
+                    <h2 className="mainBanner__content_subtitle">
+                        NEW ARRIVALS
+                    </h2>
+                    <h1 className="mainBanner__content_title">
+                        TRENDY LOFT FURNITURE
+                    </h1>
+                    <p className="mainBanner__content_text">
+                        Відчуйте привабливість нашої колекції Trendy Loft
+                        Furniture, яка ідеально поєднує в собі елегантність і
+                        сучасність.{' '}
+                    </p>
+                    <a href="/">
+                        <button
+                            className="mainBanner__content_button"
+                            type="button"
+                        >
+                            Детальніше
+                        </button>
+                    </a>
+                </div>
             </div>
         </div>
     );
