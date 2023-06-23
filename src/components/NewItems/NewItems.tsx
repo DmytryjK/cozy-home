857d3aac123a7caef58f77a45a7eb851ee92067e
DmytryjK <dmytryj.kyzyma@gmail.com>
2023-06-23 17:20:37 +0300

    FEAT: add slider for NewItems block
    Jira: [[FE] Slider for NewItems block](https://cozyhometeam.atlassian.net/browse/CH-67)

--- src/components/NewItems/NewItems.tsx
+++ src/components/NewItems/NewItems.tsx
@@ -1,3 +1,5 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import nextId from 'react-id-generator';
 import ProductCard from '../ProductCard/ProductCard';
 import './NewItems.scss';
 
@@ -11,19 +13,53 @@ const NewItems = () => {
                         Дивитись всі
                     </a>
                 </div>
+            </div>
+            <div className="container container_pd-right-off">
                 <ul className="new-items__cards">
-                    <li className="new-items__card">
-                        <ProductCard />
-                    </li>
-                    <li className="new-items__card">
-                        <ProductCard />
-                    </li>
-                    <li className="new-items__card">
-                        <ProductCard />
-                    </li>
-                    <li className="new-items__card">
-                        <ProductCard />
-                    </li>
+                    <Swiper
+                        className="new-items__slider"
+                        spaceBetween={32}
+                        breakpoints={{
+                            340: {
+                                width: 304,
+                                slidesPerView: 1,
+                                spaceBetween: '24px',
+                            },
+                            640: {
+                                width: 710,
+                                slidesPerView: 2,
+                            },
+                            1024: {
+                                width: 985,
+                                slidesPerView: 3,
+                            },
+                            1352: {
+                                width: 1310,
+                                slidesPerView: 4,
+                            },
+                        }}
+                    >
+                        <SwiperSlide key={nextId('card-of-newItems')}>
+                            <li className="new-items__card">
+                                <ProductCard />
+                            </li>
+                        </SwiperSlide>
+                        <SwiperSlide key={nextId('card-of-newItems')}>
+                            <li className="new-items__card">
+                                <ProductCard />
+                            </li>
+                        </SwiperSlide>
+                        <SwiperSlide key={nextId('card-of-newItems')}>
+                            <li className="new-items__card">
+                                <ProductCard />
+                            </li>
+                        </SwiperSlide>
+                        <SwiperSlide key={nextId('card-of-newItems')}>
+                            <li className="new-items__card">
+                                <ProductCard />
+                            </li>
+                        </SwiperSlide>
+                    </Swiper>
                 </ul>
             </div>
         </section>
