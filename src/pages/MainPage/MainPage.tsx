26310d427cfcfffa1f5d9406d2a64ffcecd03666
Andrii Hlukhov <110923640+andriihlukhov@users.noreply.github.com>
2023-06-23 19:13:17 +0300

    FEAT(home-page): Catalogue component created (#19)
    Jira: [[FE] Catalogue component for MainPage](https://cozyhometeam.atlassian.net/browse/CH-78)

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,12 +3,14 @@ import PopularItems from '../../components/PopularItems/PopularItems';
 import MainBanner from '../../components/MainBanner/MainBanner';
 import NewItems from '../../components/NewItems/NewItems';
 import './MainPage.scss';
+import CatalogueBlock from '../../components/CatalogueBlock/CatalogueBlock';
 
 const MainPage = () => {
     return (
         <div>
             <Header />
             <MainBanner />
+            <CatalogueBlock />
             <NewItems />
             <PopularItems />
         </div>
