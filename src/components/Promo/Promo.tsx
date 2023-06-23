ef69cbca894638ba52d520d0ad3c13acc383f711
Dmytryj Kyzyma <48493462+DmytryjK@users.noreply.github.com>
2023-06-23 23:24:58 +0300

    FEAT(home-page): create Promo block component (#24)
    Jira: [[FE] Promo block component](https://cozyhometeam.atlassian.net/browse/CH-68)

--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -1,7 +1,80 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import nextId from 'react-id-generator';
+import promoImg1 from '../../assets/images/promo/promo-1.jpg';
+import promoImg2 from '../../assets/images/promo/promo-2.jpg';
+import 'swiper/css/pagination';
 import './Promo.scss';
 
 const Promo = () => {
-    return <div>Promo</div>;
+    return (
+        <section className="promo">
+            <div className="container container_pd-off">
+                <Swiper
+                    className="promo__slider"
+                    spaceBetween={32}
+                    modules={[Pagination]}
+                    pagination={{ clickable: false }}
+                    breakpoints={{
+                        340: {
+                            slidesPerView: 'auto',
+                            spaceBetween: '12px',
+                            pagination: {
+                                clickable: true,
+                            },
+                        },
+                        550: {
+                            width: 580,
+                            slidesPerView: 'auto',
+                            spaceBetween: '24px',
+                        },
+                        768: {
+                            width: 710,
+                            slidesPerView: 'auto',
+                        },
+                        1160: {
+                            slidesPerView: 2,
+                        },
+                    }}
+                >
+                    <SwiperSlide key={nextId('promo-item')}>
+                        <a className="promo__add-link add-link" href="/">
+                            <h2 className="add-link__title">
+                                Vintage Loft Heritage Collection
+                            </h2>
+                            <span className="add-link__decorative">
+                                <span className="add-link__decorative-text">
+                                    Детальніше
+                                </span>
+                            </span>
+                            <img
+                                className="add-link__bg-img"
+                                src={promoImg1}
+                                alt="Vintage Loft Heritage Collection"
+                            />
+                        </a>
+                    </SwiperSlide>
+                    <SwiperSlide key={nextId('card-of-newItems')}>
+                        <a className="promo__add-link add-link" href="/">
+                            <h2 className="add-link__title">
+                                Urban Industrial Living
+                            </h2>
+                            <span className="add-link__decorative">
+                                <span className="add-link__decorative-text">
+                                    Детальніше
+                                </span>
+                            </span>
+                            <img
+                                className="add-link__bg-img"
+                                src={promoImg2}
+                                alt="Vintage Loft Heritage Collection"
+                            />
+                        </a>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+        </section>
+    );
 };
 
 export default Promo;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,6 +2,7 @@ import Header from '../../components/Header/Header';
 import PopularItems from '../../components/PopularItems/PopularItems';
 import MainBanner from '../../components/MainBanner/MainBanner';
 import NewItems from '../../components/NewItems/NewItems';
+import Promo from '../../components/Promo/Promo';
 import './MainPage.scss';
 import CatalogueBlock from '../../components/CatalogueBlock/CatalogueBlock';
 
@@ -13,6 +14,7 @@ const MainPage = () => {
             <CatalogueBlock />
             <NewItems />
             <PopularItems />
+            <Promo />
         </div>
     );
 };
