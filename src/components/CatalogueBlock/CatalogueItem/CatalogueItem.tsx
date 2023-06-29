5f84080d01aa80c2868878c0315b31a52f56f57e
andriihlukhov <andreyglykhov411@gmail.com>
2023-06-29 23:40:02 +0300

    FEAT(home-page): Adaptive for CatalogueBlock
    Jira: [[FE] Adaptive Catalogue block](https://cozyhometeam.atlassian.net/browse/CH-110)

--- src/components/CatalogueBlock/CatalogueItem/CatalogueItem.tsx
+++ src/components/CatalogueBlock/CatalogueItem/CatalogueItem.tsx
@@ -4,12 +4,13 @@ type Props = {
     src: string;
     alt: string;
     title: string;
+    className: string;
 };
 
 const CatalogueItem = (props: Props) => {
-    const { src, alt, title } = props;
+    const { src, alt, title, className } = props;
     return (
-        <a href="/" className="catalogue__item">
+        <a href="/" className={className}>
             <img src={src} alt={alt} className="catalogue__item_img" />
             <div className="catalogue__item_content">
                 <h2 className="catalogue__item_content_title">{title}</h2>
