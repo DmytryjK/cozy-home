c65e8f9bf47653f85ac6a212eec779a6f01cdb56
DmytryjK <dmytryj.kyzyma@gmail.com>
2023-07-04 16:49:18 +0300

    FIX(home-page): Compress images in Catalog block
    Jira: [[FE] Compress images in Catalog block](https://cozyhometeam.atlassian.net/browse/CH-131)

--- src/components/CatalogueBlock/CatalogueBlock.tsx
+++ src/components/CatalogueBlock/CatalogueBlock.tsx
@@ -5,6 +5,12 @@ import Tables from '../../assets/images/catalogue/image_3.png';
 import Dressers from '../../assets/images/catalogue/image_4.png';
 import Сhairs_v2 from '../../assets/images/catalogue/image_5.png';
 import Beds from '../../assets/images/catalogue/image_6.png';
+import SofasWebp from '../../assets/images/catalogue/image_1.webp';
+import ChairsWebp from '../../assets/images/catalogue/image_2.webp';
+import TablesWebp from '../../assets/images/catalogue/image_3.webp';
+import DressersWebp from '../../assets/images/catalogue/image_4.webp';
+import Сhairs_v2Webp from '../../assets/images/catalogue/image_5.webp';
+import BedsWebp from '../../assets/images/catalogue/image_6.webp';
 import CatalogueItem from './CatalogueItem/CatalogueItem';
 
 const CatalogueBlock = () => {
@@ -14,37 +20,43 @@ const CatalogueBlock = () => {
                 <CatalogueItem
                     title="Дивани"
                     alt="Дивани"
-                    src={Sofas}
+                    srcImg={Sofas}
+                    srcWebp={SofasWebp}
                     className="catalogue__item item1"
                 />
                 <CatalogueItem
                     title="Крісла"
                     alt="Крісла"
-                    src={Chairs}
+                    srcImg={Chairs}
+                    srcWebp={ChairsWebp}
                     className="catalogue__item item2"
                 />
                 <CatalogueItem
                     title="Столи"
                     alt="Столи"
-                    src={Tables}
+                    srcImg={Tables}
+                    srcWebp={TablesWebp}
                     className="catalogue__item item3"
                 />
                 <CatalogueItem
                     title="Комоди"
                     alt="Комоди"
-                    src={Dressers}
+                    srcImg={Dressers}
+                    srcWebp={DressersWebp}
                     className="catalogue__item item4"
                 />
                 <CatalogueItem
                     title="Стільці"
                     alt="Стільці"
-                    src={Сhairs_v2}
+                    srcImg={Сhairs_v2}
+                    srcWebp={Сhairs_v2Webp}
                     className="catalogue__item item5"
                 />
                 <CatalogueItem
                     title="Ліжка"
                     alt="Ліжка"
-                    src={Beds}
+                    srcImg={Beds}
+                    srcWebp={BedsWebp}
                     className="catalogue__item item6"
                 />
             </div>
--- src/components/CatalogueBlock/CatalogueItem/CatalogueItem.tsx
+++ src/components/CatalogueBlock/CatalogueItem/CatalogueItem.tsx
@@ -1,17 +1,25 @@
 import arrowIcon from '../../../assets/icons/catalogue/arrow-catalogue.svg';
 
 type Props = {
-    src: string;
+    srcImg: string;
+    srcWebp: string;
     alt: string;
     title: string;
     className: string;
 };
 
 const CatalogueItem = (props: Props) => {
-    const { src, alt, title, className } = props;
+    const { srcImg, srcWebp, alt, title, className } = props;
     return (
         <a href="/" className={className}>
-            <img src={src} alt={alt} className="catalogue__item_img" />
+            <picture>
+                <source
+                    className="catalogue__item_img"
+                    type="image/webp"
+                    srcSet={srcWebp}
+                />
+                <img className="catalogue__item_img" src={srcImg} alt={alt} />
+            </picture>
             <div className="catalogue__item_content">
                 <h2 className="catalogue__item_content_title">{title}</h2>
                 <img src={arrowIcon} alt="Детальніше" />
