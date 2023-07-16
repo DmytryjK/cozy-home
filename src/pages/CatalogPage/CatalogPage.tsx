bfbb4783fd762b31e3fee6b0f094b2a9ef30a661
Dmytryj Kyzyma <48493462+DmytryjK@users.noreply.github.com>
2023-07-16 16:00:08 +0300

    FEAT(catalog-page): create Sort component (#104)
    Jira: [[FE]create Sort component](https://cozyhometeam.atlassian.net/browse/CH-216)

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -2,13 +2,26 @@ import { Header } from '../../shared-components/Header';
 import Footer from '../../shared-components/Footer/Footer';
 import CategoryList from './components/CategoryList/CategoryList';
 import Filters from './components/Filters/Filters';
+import SortProducts from './components/SortProducts/SortProducts';
+import './CatalogPage.scss';
 
 const CatalogPage = () => {
     return (
         <>
             <Header />
             <CategoryList />
-            <Filters />
+            <section className="catalog-content">
+                <div className="container container_content-wrapper ">
+                    <Filters />
+                    <div className="catalog__main-content main-content">
+                        <div className="main-content__top">
+                            <SortProducts />
+                        </div>
+                        <div className="main-content__products">dsfasdfa</div>
+                        <div className="main-content__pagination">asdfasf</div>
+                    </div>
+                </div>
+            </section>
             <Footer />
         </>
     );
---
+++ src/pages/CatalogPage/components/SortProducts/SortProducts.tsx
@@ -0,0 +1,105 @@
+import { useState, useEffect, FormEvent } from 'react';
+import './SortProducts.scss';
+
+const SortProducts = () => {
+    const [isActive, setIsActive] = useState<boolean>(false);
+    const [currentSortOption, setCurrentSortOption] = useState<string>('');
+
+    const closeSelect = (e: any) => {
+        if (
+            !e.target.closest('.sort__custom-fields') &&
+            !e.target.closest('.sort__custom-select')
+        ) {
+            setIsActive(false);
+        }
+    };
+
+    useEffect(() => {
+        if (isActive) {
+            document.addEventListener('click', closeSelect);
+        }
+
+        return () => document.removeEventListener('click', closeSelect);
+    }, [isActive]);
+
+    const handleChangeInputs = (event: FormEvent<HTMLFieldSetElement>) => {
+        const target = event.target as HTMLInputElement;
+        setCurrentSortOption(target.value);
+    };
+
+    return (
+        <div className="main-content__sort sort">
+            <div className={`sort__custom-select ${isActive ? 'active' : ''}`}>
+                <button
+                    className="sort__open-btn"
+                    type="button"
+                    onClick={() => setIsActive(!isActive)}
+                >
+                    <span>Сортувати</span>{' '}
+                    {currentSortOption || 'за популярністю'}
+                </button>
+                <fieldset
+                    className="sort__custom-fields"
+                    onChange={handleChangeInputs}
+                >
+                    <div className="sort__field">
+                        <input
+                            className="sort__input"
+                            id="input-ascending"
+                            type="radio"
+                            name="sort"
+                            value="від дешевих до дорогих"
+                        />
+                        <label
+                            className="sort__label"
+                            htmlFor="input-ascending"
+                        >
+                            від дешевих до дорогих
+                        </label>
+                    </div>
+                    <div className="sort__field">
+                        <input
+                            className="sort__input"
+                            id="input-descending"
+                            type="radio"
+                            name="sort"
+                            value="від дорогих до дешевих"
+                        />
+                        <label
+                            className="sort__label"
+                            htmlFor="input-descending"
+                        >
+                            від дорогих до дешевих
+                        </label>
+                    </div>
+                    <div className="sort__field">
+                        <input
+                            className="sort__input"
+                            id="input-popular"
+                            type="radio"
+                            name="sort"
+                            value="за популярністю"
+                        />
+                        <label className="sort__label" htmlFor="input-popular">
+                            за популярністю
+                        </label>
+                    </div>
+                    <div className="sort__field">
+                        <input
+                            className="sort__input"
+                            id="input-rating"
+                            type="radio"
+                            name="sort"
+                            value="за рейтингом"
+                        />
+                        <label className="sort__label" htmlFor="input-rating">
+                            за рейтингом
+                        </label>
+                    </div>
+                </fieldset>
+            </div>
+        </div>
+    );
+};
+
+export default SortProducts;
