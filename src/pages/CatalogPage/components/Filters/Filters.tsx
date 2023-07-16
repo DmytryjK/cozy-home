9de6423aa41073841538d71cf5ef6c7091d4425a
Dmytryj Kyzyma <48493462+DmytryjK@users.noreply.github.com>
2023-07-16 11:38:45 +0300

    FEAT(catalog-page): create RangeFilter component (#102)
    Jira: [[FE] RangeFilter component](https://cozyhometeam.atlassian.net/browse/CH-220)

--- src/pages/CatalogPage/components/Filters/Filters.tsx
+++ src/pages/CatalogPage/components/Filters/Filters.tsx
@@ -1,11 +1,13 @@
 import ColorFilter from './ColorFilter/ColorFilter';
 import './Filters.scss';
+import RangeFilter from './RangeFilter/RangeFilter';
 
 const Filters = () => {
     return (
         <div className="container">
             <div className="filters">
                 <ColorFilter />
+                <RangeFilter minValue={20} maxValue={50} title="Ціна (грн)" />
             </div>
         </div>
     );
