c8e10b6365478214f3f207703b5b5a87836577c9
DmytryjK <dmytryj.kyzyma@gmail.com>
2023-07-17 12:52:39 +0300

    FEAT(catalog-page): create Pagination component
    Jira: [[FE]Pagination component](https://cozyhometeam.atlassian.net/browse/CH-221)

---
+++ src/pages/CatalogPage/components/Pagination/Pagination.tsx
@@ -0,0 +1,108 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import { useState } from 'react';
+import nextId from 'react-id-generator';
+import paginationSprite from '../../../../assets/icons/pagination/pagination-sprites.svg';
+import './Pagination.scss';
+
+const Pagination = () => {
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    const handleCurrentPage = (e: React.MouseEvent<HTMLUListElement>) => {
+        const target = e.target as HTMLButtonElement;
+        if (target.type === 'button') {
+            const page = target.getAttribute('data-value');
+            setCurrentPage(page ? +page : currentPage);
+        }
+    };
+
+    const getDots = () => {
+        return (
+            <li className="pagination__item" key={nextId('page-dots')}>
+                <span className="pagination__dots">...</span>
+            </li>
+        );
+    };
+
+    const getPages = (page: number) => {
+        return (
+            <li className="pagination__item" key={nextId('page-')}>
+                <button
+                    className={`pagination__page-btn ${
+                        currentPage === page ? 'active' : ''
+                    }`}
+                    type="button"
+                    data-value={page}
+                >
+                    {page}
+                </button>
+            </li>
+        );
+    };
+
+    const renderPagination = () => {
+        return pages.map((page, index) => {
+            if (currentPage < 4) {
+                if (index === 4 && pages.length > 5) {
+                    return getDots();
+                }
+                if (index > 4 && index < pages.length - 1) {
+                    return '';
+                }
+            }
+            if (currentPage >= 4 && currentPage < pages.length - 2) {
+                if (
+                    index === currentPage - 3 ||
+                    (index === currentPage + 1 && index !== pages.length - 1)
+                ) {
+                    return getDots();
+                }
+                if (
+                    (index > 0 && index < currentPage - 3) ||
+                    (index > currentPage + 1 && index < pages.length - 1)
+                ) {
+                    return '';
+                }
+            }
+            if (currentPage >= pages.length - 2 && pages.length > 5) {
+                if (index === 1) {
+                    return getDots();
+                }
+                if (index > 0 && index < pages.length - 4) {
+                    return '';
+                }
+            }
+            return getPages(page);
+        });
+    };
+
+    return (
+        <div className="main-content__pagination pagination">
+            <ul className=" pagination__list" onClick={handleCurrentPage}>
+                {renderPagination()}
+            </ul>
+            <button
+                className="pagination__prev-btn"
+                type="button"
+                onClick={() => setCurrentPage(currentPage - 1)}
+                disabled={currentPage <= 1}
+            >
+                <svg width={24} height={24} className="prev-btn__icon">
+                    <use href={`${paginationSprite}#prev`} />
+                </svg>
+            </button>
+            <button
+                className="pagination__next-btn"
+                type="button"
+                onClick={() => setCurrentPage(currentPage + 1)}
+                disabled={currentPage >= pages.length}
+            >
+                <svg width={24} height={24} className="next-btn__icon">
+                    <use href={`${paginationSprite}#next`} />
+                </svg>
+            </button>
+        </div>
+    );
+};
+
+export default Pagination;
