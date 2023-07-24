78c2ad271781e233c42fb71b9a1cdfa9c5c45ea3
DmytryjK <dmytryj.kyzyma@gmail.com>
2023-07-24 16:41:38 +0300

    FIX(home-page): images request by click on color
    Jira: [[FE]images request by click on color](https://cozyhometeam.atlassian.net/browse/CH-260)

--- src/shared-components/ProductCard/ProductCard.tsx
+++ src/shared-components/ProductCard/ProductCard.tsx
@@ -1,45 +1,20 @@
-import { useState, useEffect, useRef } from 'react';
-import { SwiperSlide, Swiper } from 'swiper/react';
-import nextId from 'react-id-generator';
-import type swiper from 'swiper';
+import { useState, useEffect } from 'react';
 import { ProductCardType } from '../../types/types';
+import SliderImages, {
+    ImageType,
+    ImagesData,
+} from './SliderImages/SliderImages';
 import headerSprites from '../../assets/icons/header/header-sprite.svg';
-import imageNotFound from '../../assets/images/error-images/image-not-found_small.png';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import './ProductCard.scss';
 
-export type TSwiper = swiper & {
-    slides: {
-        swiperSlideSize: number;
-    }[];
-};
-
 const ProductCard = ({ product }: { product: ProductCardType }) => {
-    const [currentColor, setCurrentColor] = useState<string>('');
-    const [isColorChosen, setIsColorChosen] = useState(false);
     const [priceSpaced, setPriceSpaced] = useState<string>('');
     const [discountPriceSpaced, setDiscountPriceSpaced] = useState<string>('');
-    const uniqIdForInputName = nextId('color-');
+    const [imagesData, setImagesData] = useState<ImagesData>({});
 
-    const {
-        skuCode,
-        name,
-        shortDescription,
-        price,
-        priceWithDiscount,
-        discount,
-        imageDtoList,
-        colorDtoList,
-    } = product;
-
-    const cardSliderRef = useRef<TSwiper>();
-
-    useEffect(() => {
-        if (!isColorChosen && imageDtoList.length > 0) {
-            setCurrentColor(colorDtoList[0].name);
-        }
-    }, [isColorChosen, imageDtoList]);
+    const { price, priceWithDiscount, discount } = product;
 
     const addSpaceToPrice = (
         currentPrice: number,
@@ -61,14 +36,6 @@ const ProductCard = ({ product }: { product: ProductCardType }) => {
         addSpaceToPrice(price, priceWithDiscount);
     }, [price, priceWithDiscount]);
 
-    const handleSlideChange = (color: string, index: number, id: string) => {
-        setCurrentColor(color);
-        setIsColorChosen(true);
-        if (cardSliderRef?.current) {
-            cardSliderRef.current.slideTo(index);
-        }
-    };
-
     return (
         <div className="product-card">
             <button className="product-card__favorite-btn" type="button">
@@ -83,120 +50,11 @@ const ProductCard = ({ product }: { product: ProductCardType }) => {
             {discount ? (
                 <div className="product-card__sales-text">{discount}%</div>
             ) : null}
-            <a className="product-card__slider-link" href="/">
-                <Swiper
-                    className="product-card__slider"
-                    slidesPerView={1}
-                    navigation
-                    allowTouchMove={false}
-                    onSwiper={(swiper) => {
-                        cardSliderRef.current = swiper as TSwiper;
-                    }}
-                    onSlideChange={(swiper) => {
-                        colorDtoList?.forEach((item, index) => {
-                            if (swiper.activeIndex === index) {
-                                setCurrentColor(item.name);
-                            }
-                        });
-                        setIsColorChosen(true);
-                    }}
-                >
-                    {/* {imageDtoList.length === 0 ? (
-                        <SwiperSlide>
-                            <div className="product-card__image-wrapper">
-                                <img
-                                    className="product-card__image"
-                                    src={imageNotFound}
-                                    alt={name}
-                                />
-                            </div>
-                        </SwiperSlide>
-                    ) : (
-                        imageDtoList.map((item) => {
-                            const { imagePath } = item;
-                            return (
-                                <SwiperSlide key={nextId('productCard-slide')}>
-                                    <div className="product-card__image-wrapper">
-                                        <img
-                                            className="product-card__image"
-                                            src={
-                                                imagePath === null
-                                                    ? imageNotFound
-                                                    : imagePath
-                                            }
-                                            alt={name}
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                            );
-                        })
-                    )} */}
-                    {imageDtoList.length === 0 ? (
-                        <SwiperSlide>
-                            <div className="product-card__image-wrapper">
-                                <img
-                                    className="product-card__image"
-                                    src={imageNotFound}
-                                    alt={name}
-                                />
-                            </div>
-                        </SwiperSlide>
-                    ) : (
-                        colorDtoList.map(() => {
-                            const { imagePath } = imageDtoList[0];
-                            return (
-                                <SwiperSlide key={nextId('productCard-slides')}>
-                                    <div className="product-card__image-wrapper">
-                                        <img
-                                            className="product-card__image"
-                                            src={imagePath}
-                                            alt={name}
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                            );
-                        })
-                    )}
-                </Swiper>
-            </a>
-            <div className="product-card__content swiper-no-swiping">
-                <div className="product-card__content-top">
-                    <h2 className="product-card__title">
-                        <a className="product-card__title-link" href="/">
-                            {name}
-                        </a>
-                    </h2>
-                    <fieldset className="product-card__color-checkboxes">
-                        {colorDtoList.map((color, index) => {
-                            const { name, id } = color;
-                            return (
-                                <label
-                                    className="product-card__checkbox-label"
-                                    key={nextId('color-radio')}
-                                >
-                                    <input
-                                        className={`product-card__color-checkbox ${name}`}
-                                        type="radio"
-                                        name={uniqIdForInputName}
-                                        value={name}
-                                        checked={
-                                            currentColor === name ||
-                                            (!isColorChosen && index === 0)
-                                        }
-                                        onChange={() =>
-                                            handleSlideChange(name, index, id)
-                                        }
-                                    />
-                                    <span
-                                        className={`product-card__checked-checkbox ${name}`}
-                                    />
-                                </label>
-                            );
-                        })}
-                    </fieldset>
-                </div>
-                <p className="product-card__description">{shortDescription}</p>
-            </div>
+            <SliderImages
+                productData={product}
+                setImagesData={setImagesData}
+                imagesData={imagesData}
+            />
             <div className="product-card__purchase-block purchase-block swiper-no-swiping">
                 <div
                     className={
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import popularItemsReducer from './reducers/popularItemsSlice';
 import newItemsReducer from './reducers/newItemsSlice';
 import catalogFiltersReducer from './reducers/catalogFilterSlice';
+import productCardReducer from './reducers/productCardSlice';
 
 export const store = configureStore({
     reducer: {
         popularItems: popularItemsReducer,
         newItems: newItemsReducer,
         catalogFilters: catalogFiltersReducer,
+        productCard: productCardReducer,
     },
 });
 
