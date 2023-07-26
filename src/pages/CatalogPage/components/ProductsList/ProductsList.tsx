d8bb81caf8701bdfd52950ea69eb1c5cc0f8739d
Dmytryj Kyzyma <48493462+DmytryjK@users.noreply.github.com>
2023-07-26 14:21:12 +0300

    FEAT(catalog-page): create filters by colors with API (#139)
    Jira: [[FE] create filters by colors with API](https://cozyhometeam.atlassian.net/browse/CH-273)

--- src/pages/CatalogPage/components/ProductsList/ProductsList.tsx
+++ src/pages/CatalogPage/components/ProductsList/ProductsList.tsx
@@ -1,26 +1,38 @@
 import React, { useEffect } from 'react';
 import nextId from 'react-id-generator';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/hooks';
-import { fetchCatalogProductsByFilters } from '../../../../store/reducers/catalogFilterSlice';
+import { fetchCatalogProductsByFilters } from '../../../../store/reducers/catalogProductsSlice';
 import ProductCard from '../../../../shared-components/ProductCard/ProductCard';
 import renderServerData from '../../../../helpers/renderServerData';
 import './ProductsList.scss';
 
 const ProductsList = () => {
     const dispatch = useAppDispatch();
-    const { catalogProducts, error, loading, globalFiltersQuery } =
-        useAppSelector((state) => state.catalogFilters);
-    const { id, page } = globalFiltersQuery;
+    const { catalogProducts, error, loading } = useAppSelector(
+        (state) => state.catalogProducts
+    );
+    const endpoint = useAppSelector(
+        (state) => state.catalogFilters.globalFiltersQuery.extraEndpoint
+    );
+    const categoryId = useAppSelector(
+        (state) => state.catalogFilters.globalFiltersQuery.id
+    );
+    const currentPage = useAppSelector(
+        (state) => state.catalogFilters.globalFiltersQuery.page
+    );
 
     useEffect(() => {
-        if (id) {
+        if (categoryId) {
             dispatch(
                 fetchCatalogProductsByFilters({
-                    ...globalFiltersQuery,
+                    extraEndpoint: endpoint,
+                    id: categoryId,
+                    page: currentPage,
+                    size: '12',
                 })
             );
         }
-    }, [id, page]);
+    }, [categoryId, currentPage]);
 
     const items = () => {
         return catalogProducts.map((catalogProduct) => {
--- src/store/reducers/catalogFilterSlice.ts
+++ src/store/reducers/catalogFilterSlice.ts
@@ -1,64 +1,21 @@
-import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
-import { ProductCardType, Loading } from '../../types/types';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { CatalogFilterParametersType } from '../../types/types';
 import { GlobalFiltersQuery } from '../../types/catalogFiltersTypes';
-import API_BASE from '../../utils/API_BASE';
 
 interface CatalogFilterState {
-    catalogProducts: ProductCardType[];
+    catalogFilterParametersDto: CatalogFilterParametersType | null;
     globalFiltersQuery: GlobalFiltersQuery;
     isFiltersActive: boolean;
-    loading: Loading;
-    error: null | unknown;
 }
 
 const initialState: CatalogFilterState = {
-    catalogProducts: [],
+    catalogFilterParametersDto: null,
     globalFiltersQuery: {
         page: '0',
         size: '12',
     },
     isFiltersActive: false,
-    loading: 'idle',
-    error: null,
 };
-
-export const fetchCatalogProductsByFilters = createAsyncThunk(
-    'catalogFilter/fetchCatalogProductsByFilters',
-    async function (
-        globalFiltersQuery: GlobalFiltersQuery,
-        { rejectWithValue }
-    ) {
-        try {
-            const { extraEndpoint } = globalFiltersQuery;
-            const cloneGlobalQuery = { ...globalFiltersQuery };
-            delete cloneGlobalQuery.extraEndpoint;
-
-            const filteredObjectQueries: { [key: string]: any } = {};
-            Object.entries(cloneGlobalQuery).forEach(
-                ([key, value]: [key: string, value: string]) => {
-                    if (value !== '') {
-                        filteredObjectQueries[key] = value;
-                    }
-                }
-            );
-
-            const queryParams = new URLSearchParams({
-                ...filteredObjectQueries,
-            });
-            const response = await fetch(
-                `${API_BASE()}product/${extraEndpoint}${queryParams}`
-            );
-
-            const result = await response.json();
-
-            if (!response.ok) throw new Error('something went wrong');
-
-            return result;
-        } catch (error: unknown) {
-            return rejectWithValue(error);
-        }
-    }
-);
 
 export const catalogFilterSlice = createSlice({
     name: 'catalogFilter',
@@ -76,26 +33,6 @@ export const catalogFilterSlice = createSlice({
         resetFilters(state, action: PayloadAction<boolean>) {
             state.isFiltersActive = action.payload;
         },
-    },
-    extraReducers: (builder) => {
-        builder.addCase(fetchCatalogProductsByFilters.pending, (state) => {
-            state.loading = 'pending';
-            state.error = null;
-        });
-        builder.addCase(
-            fetchCatalogProductsByFilters.fulfilled,
-            (state, action: PayloadAction<ProductCardType[]>) => {
-                state.loading = 'succeeded';
-                state.catalogProducts = action.payload;
-            }
-        );
-        builder.addCase(
-            fetchCatalogProductsByFilters.rejected,
-            (state, action: PayloadAction<unknown>) => {
-                state.loading = 'failed';
-                state.error = action.payload;
-            }
-        );
     },
 });
 
