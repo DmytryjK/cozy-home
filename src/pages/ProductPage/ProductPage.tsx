09c99b22a2420559b40e109ed8b246b45194a810
DmytryjK <dmytryj.kyzyma@gmail.com>
2023-08-09 14:35:53 +0300

    FEAT(product-page): StarsIndicator component, link to review feedbacks, SKUCode
    Jira: [[FE]Create StarsIndicator component, add link to review feedbacks, also add SKUCode of product]

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -2,7 +2,9 @@ import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { fetchMightBeInterestProducts } from '../../store/reducers/productsSliderSlice';
 import Breadcrumbs from '../../shared-components/Breadcrumbs/Breadcrumbs';
+import ProductRating from './components/ProductRating/ProductRating';
 import ProductsSlider from '../../shared-components/ProductsSlider/ProductsSlider';
+import './ProductPage.scss';
 
 const ProductPage = () => {
     const dispatch = useAppDispatch();
@@ -18,6 +20,13 @@ const ProductPage = () => {
             <Breadcrumbs />
             <div className="container">
                 <h1 className="product-page__title">Крісло COMFORT</h1>
+                <div className="product-page__extra-info">
+                    <p className="product-page__sku">240003</p>
+                    <ProductRating />
+                    <a className="product-page__feedbacks-link" href="/">
+                        2 відгука
+                    </a>
+                </div>
             </div>
 
             <ProductsSlider
