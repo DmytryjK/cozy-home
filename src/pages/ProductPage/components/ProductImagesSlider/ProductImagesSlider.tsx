61ccb9a080f5f959a6f12e885672658625e3b492
Andrii Hlukhov <110923640+andriihlukhov@users.noreply.github.com>
2023-08-11 20:55:16 +0300

    FEAT(product-page): ProductImagesSlider component created (#203)
    Jira: [[FE] Create ProductImages component](https://cozyhometeam.atlassian.net/browse/CH-392)

---
+++ src/pages/ProductPage/components/ProductImagesSlider/ProductImagesSlider.tsx
@@ -0,0 +1,61 @@
+import './ProductImagesSlider.scss';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Thumbs } from 'swiper';
+
+import nextId from 'react-id-generator';
+import { useState } from 'react';
+import productImages from '../../../../assets/images/product-page';
+
+const ProductImagesSlider = () => {
+    const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
+    const [activeIndex, setActiveIndex] = useState<number>(0);
+
+    return (
+        <div className="product-images">
+            <div className="product-images__main-image">
+                <Swiper
+                    thumbs={{ swiper: thumbsSwiper }}
+                    loop
+                    navigation
+                    modules={[Navigation, Thumbs]}
+                    grabCursor
+                    className="product-images__slider"
+                >
+                    {productImages.map((image) => (
+                        <SwiperSlide key={nextId('swiper-image')}>
+                            <img src={image} alt="Slider images" />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <Swiper
+                    onSwiper={setThumbsSwiper}
+                    loop
+                    spaceBetween={10}
+                    slidesPerView={4}
+                    modules={[Navigation, Thumbs]}
+                    className="product-images__slider-thumbs"
+                >
+                    {productImages.map((image, index) => (
+                        <SwiperSlide
+                            key={nextId('swiper-image')}
+                            className={`${
+                                activeIndex === index
+                                    ? 'swiper-slide-thumb-active'
+                                    : ''
+                            }`}
+                        >
+                            <div className="product-images__slider-thumbs-wrapper">
+                                <img src={image} alt="Slider images" />
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </div>
+    );
+};
+
+export default ProductImagesSlider;
