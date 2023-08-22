35483826387e927c0ff02d671f61dc6ff546bd9c
Andrii Hlukhov <110923640+andriihlukhov@users.noreply.github.com>
2023-08-22 20:32:58 +0300

    FEAT(product-page): CustomersReviewPopUp UI (#224)
    Jira: [[FE] Make review pop up](https://cozyhometeam.atlassian.net/browse/CH-466)

---
+++ src/pages/ProductPage/components/CustomersReviewSlider/CommentTextArea/CommentTextarea.tsx
@@ -0,0 +1,43 @@
+import React, { useState, ChangeEvent, useEffect } from 'react';
+import './CommentTextarea.scss';
+
+const CommentTextarea: React.FC = () => {
+    const [comment, setComment] = useState<string>('');
+    const [isMaxCharactersExceeded, setIsMaxCharactersExceeded] =
+        useState(false);
+    const maxLength = 500;
+
+    const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        const inputText = e.target.value;
+
+        if (inputText.length <= maxLength) {
+            setComment(inputText);
+        }
+    };
+
+    useEffect(() => {
+        setIsMaxCharactersExceeded(comment.length >= maxLength);
+    }, [comment]);
+
+    return (
+        <div className="comment-textarea">
+            <textarea
+                cols={30}
+                rows={10}
+                className="customers-review__modal_inputs_input_comment"
+                placeholder="Ваш комментарий"
+                value={comment}
+                onChange={handleTextareaChange}
+            />
+            <div
+                className={`character-count ${
+                    isMaxCharactersExceeded ? 'exceeded' : ''
+                }`}
+            >
+                {comment.length}/{maxLength}
+            </div>
+        </div>
+    );
+};
+
+export default CommentTextarea;
