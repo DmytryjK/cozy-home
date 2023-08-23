50c1f753c866e3e15b3ab9000b69f8c15fc3c687
DmytryjK <dmytryj.kyzyma@gmail.com>
2023-08-23 11:41:13 +0300

    hotfix scroll up position

--- src/utils/moveUserToPageUp.ts
+++ src/utils/moveUserToPageUp.ts
@@ -1,7 +1,7 @@
 const moveUserToPageUp = () => {
     const catalogProductsHtml = document.getElementById('catalog-content');
     if (!catalogProductsHtml) return;
-    const yOffset = window.screen.width > 991 ? -70 : -50;
+    const yOffset = window.screen.width > 991 ? -80 : -50;
     const y =
         catalogProductsHtml.getBoundingClientRect().top +
         window.pageYOffset +
