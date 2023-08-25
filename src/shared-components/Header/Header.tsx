22210b9803355e8e3aecbfb0d8a664f98d8f30d5
DmytryjK <dmytryj.kyzyma@gmail.com>
2023-08-25 11:47:23 +0300

    add number of products to cart icon

--- src/shared-components/Header/Header.tsx
+++ src/shared-components/Header/Header.tsx
@@ -188,7 +188,7 @@ const Header = () => {
                         <svg width="21" height="21">
                             <use href={`${headerSprite}#card-icon`} />
                         </svg>
-                        <span className="header__icons_cart-counter">0</span>
+                        <span className="header__icons_cart-counter">9</span>
                     </a>
                 </div>
                 <div className="header__mobile_icons">
