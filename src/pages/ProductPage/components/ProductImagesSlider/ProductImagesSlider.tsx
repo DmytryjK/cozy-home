16aaba91867982f3b99daf7dabcb70d91a3cf011
andriihlukhov <andreyglykhov411@gmail.com>
2023-08-25 16:27:12 +0300

    FIX(product-page): Fix image-slider functionallity
    Jira: [[FE] Fix functionallity for Enlarged images](https://cozyhometeam.atlassian.net/browse/CH-452)
    Jira: [[FE] Fix ProductImageSlider component](https://cozyhometeam.atlassian.net/browse/CH-495)

--- src/pages/ProductPage/components/ProductImagesSlider/ProductImagesSlider.tsx
+++ src/pages/ProductPage/components/ProductImagesSlider/ProductImagesSlider.tsx
@@ -3,72 +3,100 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Thumbs, FreeMode } from 'swiper';
+import { Navigation, Thumbs, Controller } from 'swiper';
 
 import nextId from 'react-id-generator';
 import { useState } from 'react';
 import { useAppSelector } from '../../../../hooks/hooks';
+import EnlargedPhoto from './EnlargedPhoto/EnlargedPhoto';
 
-type Props = {
-    setlargePhotoActive: React.Dispatch<React.SetStateAction<boolean>>;
-};
+const ProductImagesSlider = () => {
+    const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
+    const [largePhotoActive, setLargePhotoActive] = useState<boolean>(false);
+    const [firstSwiper, setFirstSwiper] = useState(null);
+    const [secondSwiper, setSecondSwiper] = useState(null);
+    const [activeIndex, setActiveIndex] = useState<number>(0);
 
-const ProductImagesSlider = (props: Props) => {
-    const { setlargePhotoActive } = props;
     const { images } = useAppSelector(
         (state) => state.productInformation.productInfo
     );
 
-    const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
+    const handleSlideClick = (index: number) => {
+        setActiveIndex(index);
+        setLargePhotoActive(true);
+    };
+
+    const handleFirstSwiper = (swiper: any) => {
+        setFirstSwiper(swiper);
+    };
+
+    const handleSecondSwiper = (swiper: any) => {
+        setSecondSwiper(swiper);
+    };
+
+    const handleSlideChange = (swiper: any) => {
+        setActiveIndex(swiper.activeIndex);
+    };
 
     const renderSlider = () => {
         if (images.length === 0) return null;
         return (
-            <div className="product-images">
-                <div className="product-images__main-image">
-                    <Swiper
-                        loop
-                        spaceBetween={10}
-                        navigation
-                        thumbs={{ swiper: thumbsSwiper }}
-                        modules={[FreeMode, Navigation, Thumbs]}
-                        className="product-images__slider"
-                    >
-                        {images.map((image) => (
-                            <SwiperSlide
-                                key={nextId('swiper-image')}
-                                onClick={() => setlargePhotoActive(true)}
-                            >
-                                <img
-                                    src={image.desktopImagePath}
-                                    alt="Slider images"
-                                />
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                    <Swiper
-                        onSwiper={setThumbsSwiper}
-                        loop
-                        spaceBetween={10}
-                        slidesPerView={4}
-                        freeMode
-                        watchSlidesProgress
-                        modules={[FreeMode, Navigation, Thumbs]}
-                        className="product-images__slider-thumbs"
-                    >
-                        {images.map((image) => (
-                            <SwiperSlide key={nextId('swiper-image')}>
-                                <div className="product-images__slider-thumbs-wrapper">
+            <>
+                <div className="product-images">
+                    <div className="product-images__main-image">
+                        <Swiper
+                            spaceBetween={10}
+                            navigation
+                            thumbs={{ swiper: thumbsSwiper }}
+                            modules={[Navigation, Thumbs, Controller]}
+                            onSwiper={handleFirstSwiper}
+                            controller={{ control: secondSwiper }}
+                            className="product-images__slider"
+                        >
+                            {images.map((image, index) => (
+                                <SwiperSlide
+                                    key={nextId('swiper-image')}
+                                    onClick={() => handleSlideClick(index)}
+                                >
                                     <img
-                                        src={image.sliderImagePath}
+                                        src={image.desktopImagePath}
                                         alt="Slider images"
                                     />
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                        <Swiper
+                            onSwiper={setThumbsSwiper}
+                            spaceBetween={10}
+                            slidesPerView={4}
+                            watchSlidesProgress
+                            modules={[Navigation, Thumbs]}
+                            className="product-images__slider-thumbs"
+                        >
+                            {images.map((image) => (
+                                <SwiperSlide key={nextId('swiper-image')}>
+                                    <div className="product-images__slider-thumbs-wrapper">
+                                        <img
+                                            src={image.sliderImagePath}
+                                            alt="Slider images"
+                                        />
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
                 </div>
-            </div>
+                {largePhotoActive && (
+                    <EnlargedPhoto
+                        setLargePhotoActive={setLargePhotoActive}
+                        largePhotoActive={largePhotoActive}
+                        handleSecondSwiper={handleSecondSwiper}
+                        firstSwiper={firstSwiper}
+                        activeIndex={activeIndex}
+                        handleSlideChange={handleSlideChange}
+                    />
+                )}
+            </>
         );
     };
 
