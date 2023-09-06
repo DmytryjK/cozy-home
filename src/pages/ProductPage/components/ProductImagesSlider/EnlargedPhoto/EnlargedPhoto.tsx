f8884dc55191fcb40cc1e937633e8d98851184ec
andriihlukhov <andreyglykhov411@gmail.com>
2023-09-06 18:10:11 +0300

    FEAT(product-page): ProductImagesSlider pop-up adaptive fix
    Jira: [[FE] Fix adaptive for Enlarged Photo component](https://cozyhometeam.atlassian.net/browse/CH-556)

--- src/pages/ProductPage/components/ProductImagesSlider/EnlargedPhoto/EnlargedPhoto.tsx
+++ src/pages/ProductPage/components/ProductImagesSlider/EnlargedPhoto/EnlargedPhoto.tsx
@@ -53,6 +53,7 @@ const EnlargedPhoto = (props: Props) => {
         return (
             <Swiper
                 navigation
+                slidesPerView={1}
                 modules={[Navigation, Controller]}
                 onSwiper={handleSecondSwiper}
                 onSlideChange={handleSlideChange}
@@ -73,26 +74,46 @@ const EnlargedPhoto = (props: Props) => {
     };
 
     return (
-        <div className="block-wrapper">
-            <div className="enlarged-block">
-                <div className="enlarged-block__content">
-                    <div className="enlarged-block__content_header">
-                        <div className="enlarged-block__content_header_sliderCounter">
-                            <p>{`${activeIndex + 1}/${popUpImages?.length}`}</p>
-                        </div>
-                        <button
-                            type="button"
-                            className="enlarged-block__content_header_close"
-                            onClick={() => setLargePhotoActive(false)}
-                        >
-                            <svg width="40" height="40">
-                                <use href={`${productPageSprite}#close-icon`} />
-                            </svg>
-                        </button>
+        // <div className="block-wrapper">
+        //     <div className="enlarged-block">
+        //         <div className="enlarged-block__content">
+        //             <div className="enlarged-block__content_header">
+        //                 <div className="enlarged-block__content_header_sliderCounter">
+        //                     <p>{`${activeIndex + 1}/${popUpImages?.length}`}</p>
+        //                 </div>
+        //                 <button
+        //                     type="button"
+        //                     className="enlarged-block__content_header_close"
+        //                     onClick={() => setLargePhotoActive(false)}
+        //                 >
+        //                     <svg width="40" height="40">
+        //                         <use href={`${productPageSprite}#close-icon`} />
+        //                     </svg>
+        //                 </button>
+        //             </div>
+        //             {renderSlider()}
+        //         </div>
+        //     </div>
+        // </div>
+        <div className="enlarged-block">
+            <div className="enlarged-block__content">
+                <div className="enlarged-block__content_header">
+                    <div className="enlarged-block__content_header_sliderCounter">
+                        <p>{`${activeIndex + 1}/${popUpImages?.length}`}</p>
                     </div>
-                    {renderSlider()}
+
+                    <button
+                        type="button"
+                        className="enlarged-block__content_header_close"
+                        onClick={() => setLargePhotoActive(false)}
+                    >
+                        <svg width="40" height="40">
+                            <use href={`${productPageSprite}#close-icon`} />
+                        </svg>
+                    </button>
                 </div>
             </div>
+            {renderSlider()}
         </div>
     );
 };
