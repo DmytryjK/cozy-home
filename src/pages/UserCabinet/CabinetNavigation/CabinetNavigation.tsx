a1055e4eab5d02b41631d9240f55d378b60b42d9
Dmytryj Kyzyma <48493462+DmytryjK@users.noreply.github.com>
2023-10-26 14:19:16 +0300

    FEAT(user-cabinet): Cretae Navigation Panel for UserCabinet (#300)
    Jira: [[FE]Cretae Navigation Panel for UserCabinet](https://cozyhometeam.atlassian.net/browse/CH-706)

--- src/pages/UserCabinet/CabinetNavigation/CabinetNavigation.tsx
+++ src/pages/UserCabinet/CabinetNavigation/CabinetNavigation.tsx
@@ -1,8 +1,63 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import nextId from 'react-id-generator';
+import { LinkType } from '../UserCabinet';
 import './CabinetNavigation.scss';
 
 const CabinetNavigation = () => {
-    return <nav className="cabinet-navigation">sdfasdfasdf</nav>;
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
+    const listOfLinks = [
+        {
+            title: 'Контактна інформація',
+            href: '/cabinet/contacts',
+        },
+        {
+            title: 'Список бажань',
+            href: '/cabinet/favorites',
+        },
+    ];
+    const [activeLink, setActiveLink] = useState<LinkType>(listOfLinks[0]);
+
+    useEffect(() => {
+        if (pathname === '/cabinet') {
+            const name = listOfLinks[0].href;
+            navigate(`${name}`);
+        }
+    }, []);
+
+    return (
+        <nav className="cabinet-navigation">
+            <ul className="cabinet-navigation__list">
+                {listOfLinks.map((link) => {
+                    const { title, href } = link;
+                    return (
+                        <li
+                            className="cabinet-navigation__item"
+                            key={nextId(`${title}-`)}
+                        >
+                            <NavLink
+                                className="cabinet-navigation__link"
+                                to={href}
+                                onClick={() =>
+                                    setActiveLink({
+                                        title,
+                                        href,
+                                    })
+                                }
+                            >
+                                {link.title}
+                            </NavLink>
+                        </li>
+                    );
+                })}
+            </ul>
+            <NavLink className="cabinet-navigation__close" to="/">
+                Вихід
+            </NavLink>
+        </nav>
+    );
 };
 
 export default CabinetNavigation;
