6f3dbfc2f7bfcd4ee3f7380a12371b11528717e0
DmytryjK <dmytryj.kyzyma@gmail.com>
2023-10-28 14:13:40 +0300

    FEAT(user-cabinet): Create Adaptive for Navigation Panel
    Jira: [[FE]Create Adaptive for Navigation Panel](https://cozyhometeam.atlassian.net/browse/CH-716)

--- src/pages/UserCabinet/CabinetContent/CabinetContent.tsx
+++ src/pages/UserCabinet/CabinetContent/CabinetContent.tsx
@@ -1,12 +1,73 @@
-import React from 'react';
-import './CabinetContent.scss';
+import { ReactNode, useEffect, useState, useContext } from 'react';
 import UserContacts from './UserContacts/UserContacts';
 import UserFavorites from './UserFavorites/UserFavorites';
+import MobileContent from './MobileContent/MobileContent';
+import { LinkType, UserActiveLinkContext } from '../UserCabinet';
+import listOfRoutes from '../ListOfRoutes';
+import DesktopContent from './DesktopContent/DesktopContent';
+import './CabinetContent.scss';
+
+export const renderUserCabinetContent = (activeLink: LinkType | null) => {
+    let res: '' | ReactNode = '';
+    switch (activeLink?.href) {
+        case listOfRoutes[0].href:
+            res = <UserContacts />;
+            break;
+        case listOfRoutes[1].href:
+            res = <UserFavorites />;
+            break;
+        default:
+            res = '';
+    }
+    return res;
+};
 
 const CabinetContent = () => {
+    const [currentScreenSize, setCurrentScreenSize] = useState<
+        'desktop' | 'mobile' | null
+    >(null);
+    const { activeLink, setActiveLink } = useContext(UserActiveLinkContext);
+
+    useEffect(() => {
+        const checkSize = () => {
+            const screenWidth = window.screen.width;
+            if (screenWidth <= 767) {
+                setCurrentScreenSize('mobile');
+            } else {
+                setCurrentScreenSize('desktop');
+            }
+        };
+
+        if (window.screen.width <= 767) {
+            setCurrentScreenSize('mobile');
+        } else {
+            setCurrentScreenSize('desktop');
+        }
+
+        window.addEventListener('resize', checkSize);
+
+        return () => window.removeEventListener('resize', checkSize);
+    }, []);
+
+    useEffect(() => {
+        if (
+            currentScreenSize === 'desktop' &&
+            (activeLink?.href === '' || !activeLink)
+        ) {
+            setActiveLink({
+                title: listOfRoutes[0].title,
+                href: listOfRoutes[0].href,
+            });
+        }
+    }, [currentScreenSize, activeLink]);
+
     return (
         <div className="cabinet-content">
-            <UserContacts />
+            {currentScreenSize === 'desktop' ? (
+                <DesktopContent />
+            ) : (
+                <MobileContent />
+            )}
         </div>
     );
 };
--- src/pages/UserCabinet/UserCabinet.tsx
+++ src/pages/UserCabinet/UserCabinet.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useState } from 'react';
+import {
+    useState,
+    createContext,
+    Dispatch,
+    SetStateAction,
+    useMemo,
+} from 'react';
 import Breadcrumbs from '../../shared-components/Breadcrumbs/Breadcrumbs';
 import CabinetNavigation from './CabinetNavigation/CabinetNavigation';
 import CabinetContent from './CabinetContent/CabinetContent';
@@ -9,15 +15,33 @@ export type LinkType = {
     href: string;
 };
 
+type ActiveLinkContextType = {
+    activeLink: LinkType | null;
+    setActiveLink: Dispatch<SetStateAction<LinkType | null>>;
+};
+
+export const UserActiveLinkContext = createContext<ActiveLinkContextType>({
+    activeLink: null,
+    setActiveLink: () => {},
+});
+
 const UserCabinet = () => {
+    const [activeLink, setActiveLink] = useState<LinkType | null>(null);
     return (
         <>
             <Breadcrumbs />
             <section className="cabinet">
                 <div className="container">
                     <div className="cabinet__wrapper">
-                        <CabinetNavigation />
-                        <CabinetContent />
+                        <UserActiveLinkContext.Provider
+                            value={useMemo(
+                                () => ({ activeLink, setActiveLink }),
+                                [activeLink, setActiveLink]
+                            )}
+                        >
+                            <CabinetNavigation />
+                            <CabinetContent />
+                        </UserActiveLinkContext.Provider>
                     </div>
                 </div>
             </section>
