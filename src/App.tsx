d1390b28bcb794a52636fc99aecda48db2c5a399
DmytryjK <dmytryj.kyzyma@gmail.com>
2024-02-19 19:53:17 +0200

    scroll to top when user change pages on the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './shared-components/Layout/Layout';
 import routes from './routes';
+import ScrollToTop from './shared-components/ScrollToTop/ScrollToTop';
 import './App.scss';
 
 const App = () => {
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.3,
+            easing: (t) => Math.min(1, 1.001 - 2 ** (-7 * t)),
+        });
+
+        function raf(time: any) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+    }, []);
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Layout>
                 <Routes>
                     {routes.map((link) => {
