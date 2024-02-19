9bf1088fe7b2e9d101e858a64ce6f20e72052286
Dmytryj Kyzyma <48493462+DmytryjK@users.noreply.github.com>
2024-02-19 19:40:13 +0200

    added smooth-scroll Lenis (#335)

--- src/shared-components/Layout/Layout.tsx
+++ src/shared-components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 import { Header } from '../Header';
 import AllModals from '../AllModals/AllModals';
 import Footer from '../Footer/Footer';
@@ -8,6 +9,24 @@ type LayoutProps = {
 };
 
 const Layout = ({ children }: LayoutProps) => {
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.3,
+            easing: (t) => Math.min(1, 1.001 - 2 ** (-7 * t)),
+        });
+
+        lenis.on('scroll', (e: any) => {
+            console.log(e);
+        });
+
+        function raf(time: any) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+    }, []);
+
     return (
         <div>
             <Header />
